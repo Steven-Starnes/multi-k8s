d8316cd75842669a827a2c4505de278af1147970
steven <scs136@txstate.edu>
2019-10-29 21:32:35 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t starnes22/multi-client:latest -t starnes22/mult-client:$SHA -f ./client/Dockerfile ./client
+docker build -t starnes22/multi-server:latest -t starnes22/mult-server:$SHA -f ./server/Dockerfile ./server
+docker build -t starnes22/multi-worker:latest -t starnes22/mult-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push starnes22/multi-client:latest
+docker push starnes22/multi-server:latest
+docker push starnes22/multi-worker:latest
+
+docker push starnes22/multi-client:$SHA
+docker push starnes22/multi-server:$SHA
+docker push starnes22/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=starnes22/multi-server:$SHA
+kubectl set image deployments/client-deployment client=starnes22/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=starnes22/multi-worker:$SHA
